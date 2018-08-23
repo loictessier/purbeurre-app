687104b606942a94c42c0d4522e4c5e27567f2ce
loictessier <loictessier.inge@gmail.com>
2018-08-23 18:23:42 +0200

    New django app openfoofacts and created models for product and category

---
+++ openfoodfacts/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField('name', max_length=200, unique=True)
+    nutriscore = models.CharField('nutriscore', max_length=1)
+    img_url = models.URLField('img_url', max_length=2100, null=True)
+    off_url = models.URLField('off_url', max_length=2100, null=True)
+    
+    class Meta:
+        verbose_name = "aliment"
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField('name', max_length=200, unique=True)
+    off_identifier = models.CharField('off_identifier', max_length=200)
+    product = models.ManyToManyField(Product, related_name='categories', blank=True)
+
+    class Meta:
+        verbose_name = "categorie"
+
+    def __str__(self):
+        return self.name
+
+# TODO favorites and user class models 
+# (user from django module for authentication)
