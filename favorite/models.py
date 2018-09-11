9a6304bda4c95c10d87736cbd52cf2d31371e9e4
loictessier <loictessier.inge@gmail.com>
2018-09-11 21:24:48 +0200

    Modification models

--- favorite/models.py
+++ favorite/models.py
@@ -1,13 +1,25 @@
 from django.db import models
-from django.contrib.auth import models.User as User
+from django.contrib.auth.models import User
 from openfoodfacts.models import Product
 
-class favorite(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+
+class Profile(models.model):
+    user = models.OneToOneField('User', on_delete=models.CASCADE)
+    # TODO avatar (+ champs supplémentaires éventuels voir doc)
+
+    class Meta:
+        verbose_name = "profil"
+
+    def __str__(self):
+        return self.user.username
+
+
+class Favorite(models.Model):
+    profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = "favori"
 
     def __str__(self):
-        return self.products
+        return self.product.name
