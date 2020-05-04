3a388e5f469bebfe6d0d5350cc015a099375aad8
Loïc Tessier <loictessier.inge@gmail.com>
2020-05-04 22:46:00 +0200

    Installed packages gunicorn and django heroku, created Procfile and modified settings for deployment

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -139,3 +140,6 @@ LOGIN_REDIRECT_URL = '/'
 # media folder
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
